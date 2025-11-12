efb461b9979c10e04037c75b4d2566161220da24
Hector Ajumado <hector.ajumado@gmail.com>
2025-11-12 17:55:38 -0400

    Agrega una nueva directiva [ngStyle] para agregar estilos a los componentes

--- src/app/ventas/ventas.ts
+++ src/app/ventas/ventas.ts
@@ -1,13 +1,21 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ventas',
-  imports: [NgClass],
+  imports: [NgClass, NgStyle],
   templateUrl: './ventas.html',
   styleUrl: './ventas.css',
 })
 export class Ventas {
-  public estado: boolean = true;
+  public estado: boolean = false;
   public mostrar: string = 'si';
+
+  estilos = {
+    'font-family': 'Courier New',
+    'background-color': 'brown',
+    'color': '#ffffff',
+    'font-weight': 'bold',
+    'padding': '10px'
+  };
 }
